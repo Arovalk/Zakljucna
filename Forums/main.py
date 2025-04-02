3c3c71d10c441a54fbea71a4518043c07a9dd643
Arovalk <jaka.klavir@gmail.com>
2025-04-02 08:20:54 +0000

    Ogrodna coda, začetki front-end in back-end

--- Forums/main.py
+++ Forums/main.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import Mapped, mapped_column
 
 
 db = SQLAlchemy()
@@ -8,59 +9,44 @@ app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///project.db"
 
 db.init_app(app)
 
+class topic(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    title: Mapped[str] = mapped_column(unique=True)
+    description: Mapped[str]
+
+class Comment(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    text: Mapped[str] = mapped_column(unique=True)
+    topicId: Mapped[str]
 
 
 
-
-@app.route("/users")
-def user_list():
-    users = db.session.execute(db.select(User).order_by(User.username)).scalars()
-    return render_template("user/list.html", users=users)
-
-@app.route("/users/create", methods=["GET", "POST"])
-def user_create():
+@app.route("/",methods=["GET", "POST"])
+def home():
     if request.method == "POST":
-        user = User(
-            username=request.form["username"],
-            email=request.form["email"],
+        # Dodamo razpravo
+        user = topic(
+            title=request.form["title"],
+            description=request.form["description"],
         )
-        db.session.add(user)
+        db.session.add(topic)
         db.session.commit()
-        return redirect(url_for("user_detail", id=user.id))
-
-    return render_template("user/create.html")
-
-@app.route("/user/<int:id>")
-def user_detail(id):
-    user = db.get_or_404(User, id)
-    return render_template("user/detail.html", user=user)
-
-@app.route("/user/<int:id>/delete", methods=["GET", "POST"])
-def user_delete(id):
-    user = db.get_or_404(User, id)
-
-    if request.method == "POST":
-        db.session.delete(user)
-        db.session.commit()
-        return redirect(url_for("user_list"))
-
-    return render_template("user/delete.html", user=user)
-
-
-@app.route("/")
-def index():
     return render_template("index.html")
 
-@app.route("/forums")
-def forums():
-    return render_template("forums.html")
+@app.route("/topic/<int:id>",methods=["GET", "POST"])
+def topic(id):
+    if request.method == "POST":
+        # Dodamo kommentar na razpravo
+        comment = Comment(
+            text=request.form["text"],
+            topicId=request.form["topicId"],
+        )
+        db.session.add(comment)
+        db.session.commit()
 
-@app.route("/login")
-def login():
-    return render_template("login.html")
 
-
-
+        # prikaži razpravo
+    return render_template("user/detail.html", user=user)
 
 
 app.run(debug=True)
