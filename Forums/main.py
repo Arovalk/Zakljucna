6bb4445acd43c8c1e7c731b5cc7826eca7dbeb5c
Arovalk <jaka.klavir@gmail.com>
2025-04-03 09:51:10 +0000

    Dodajanje podstrani za vsako temo, in prikaz posameznih tem

--- Forums/main.py
+++ Forums/main.py
@@ -9,7 +9,7 @@ app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///project.db"
 
 db.init_app(app)
 
-class topic(db.Model):
+class Topic(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True)
     title: Mapped[str] = mapped_column(unique=True)
     description: Mapped[str]
@@ -19,34 +19,45 @@ class Comment(db.Model):
     text: Mapped[str] = mapped_column(unique=True)
     topicId: Mapped[str]
 
-
+with app.app_context():
+    db.create_all()
 
 @app.route("/",methods=["GET", "POST"])
 def home():
     if request.method == "POST":
         # Dodamo razpravo
-        user = topic(
+        topic = Topic(
             title=request.form["title"],
             description=request.form["description"],
         )
         db.session.add(topic)
         db.session.commit()
-    return render_template("index.html")
+
+    topics = db.session.execute(db.select(Topic)).scalars()
+#    for topic in topics:
+#        print(topic.title, topic.description, topic.id)
+    
+    return render_template("index.html", topics=topics)
 
 @app.route("/topic/<int:id>",methods=["GET", "POST"])
 def topic(id):
     if request.method == "POST":
         # Dodamo kommentar na razpravo
         comment = Comment(
-            text=request.form["text"],
-            topicId=request.form["topicId"],
+            text=request.form["comment"],
+            topicId=id
         )
         db.session.add(comment)
         db.session.commit()
 
 
-        # prikaži razpravo
-    return render_template("user/detail.html", user=user)
+    # prikaži razpravo
+    topic = db.get_or_404(Topic, id)
+    comments = Comment.query.filter_by(topicId=id).all()
+    print(comments)
+    for comment in comments:
+        print(comment)
+    return render_template("topic.html", topic=topic)
 
 
 app.run(debug=True)
