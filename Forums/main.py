9efa27782a306ff54e11b5cce0a7db1db822fa6e
Arovalk <jaka.klavir@gmail.com>
2025-04-09 07:45:55 +0000

    začetki logina

--- Forums/main.py
+++ Forums/main.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for, session
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import Mapped, mapped_column
+from werkzeug.security import generate_password_hash, check_password_hash
 
 
 db = SQLAlchemy()
@@ -18,6 +19,20 @@ class Comment(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True)
     text: Mapped[str]
     topicId: Mapped[str]
+
+
+
+class User(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    username: Mapped[str] = mapped_column(unique=True, nullable=False)
+    password: Mapped[str] = mapped_column(nullable=False)
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
 
 with app.app_context():
     db.create_all()
@@ -58,6 +73,26 @@ def topic(id):
 #    for comment in comments:
 #        print(comment)
     return render_template("topic.html", topic=topic, comments=comments)
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        username = request.form.get("username")  
+        password = request.form.get("password")
+        print(username, password)       
+        user = User.query.filter_by(username=username).first()
+        
+        
+        
+        if user and user.check_password(password):
+            session["user_id"] = user.id
+            return redirect(url_for("home"))
+        else:
+            return render_template("login.html")
+    
+    return render_template("login.html")      
+
+
 
 
 app.run(debug=True)
